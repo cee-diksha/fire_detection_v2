54e8468e19d8c6f11df6aa9ae0ecef5e191c8add
cee-diksha <cee.diksha@gmail.com>
2025-02-17 09:44:20 +0530

    modifications

---
+++ src/components/DeviceCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeviceCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DeviceCard
--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import SettingsTable from '../../components/SettingsTable';
-import { SettingCards } from './SettingCards.js';
+import SettingsTable from './SettingsTable.js';
+import {SettingCards} from "./SettingCards.js"
 import { io } from 'socket.io-client';
 import { URL } from '../../lib/constants.js';
 

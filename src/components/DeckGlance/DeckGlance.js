efd76ba8ba16a4a9ed2d49fb087b31cbf97d064d
cee-diksha <cee.diksha@gmail.com>
2025-02-17 20:14:26 +0530

    deck issue resolved for live

--- src/components/DeckGlance/DeckGlance.js
+++ src/components/DeckGlance/DeckGlance.js
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { FIRE_TEMP, TOTAL_DECKS } from '../../libs/Constants'
 import './DeckGlance.css'
-import fakeCardData from '../../data/fakeCardData.json'
 
-const DeckGlance = () => {
-  const [cardData, setCardData] = useState(fakeCardData);
-  const danger = cardData.filter(item => item.temp >= FIRE_TEMP);
-  const [deck, setDeck] = useState(danger);
+const DeckGlance = ({data}) => {
+  const [cardData, setCardData] = useState([]);
+  const [deck, setDeck] = useState([]);
 
   useEffect(() => {
-    setDeck(danger);
-    setCardData(fakeCardData);
-  }, [fakeCardData]);
+    setCardData(data);
+  }, [data]);
+
+  useEffect(() => {
+    const danger = cardData.filter(item => item.temp >= FIRE_TEMP);
+    setDeck(danger)
+  }, [cardData])
 
   return (
     <div className='deck-gl-mn width-100'>
       {Array.from({ length: TOTAL_DECKS }).map((_,index)=>{
          const deckNo = index + 1;
-         const dangerDeckNos = deck.map(item => item.deckno);
+         const dangerDeckNos = deck.map(item => parseInt(item.deckno));
         return(
             <div key={`hm-deck-${index}`} className='deck-gl-crd flex-center-row' style={{ width: `${100 - index * 12}%`, background: dangerDeckNos.includes(deckNo) ? "red" : ""}} >
                 <p>Deck {index+1}</p>
-            </div>
+            </div> 
         )
       })}
     </div>
